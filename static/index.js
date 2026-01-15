d2857186f3757e337c0864997bf218966c62df23
Lilly Richards <lrricha5@asu.edu>
2026-01-15 15:48:32 -0700

    Added CSS and JS to properly allow adding input to the board -LR

--- static/index.js
+++ static/index.js
@@ -1,10 +1,19 @@
 const possibleElements = document.getElementsByClassName('possible');
+
 for (let i = 0; i < possibleElements.length; i++) {
     possibleElements[i].style.display = "none";
 }
 
+const inputElements = document.getElementsByClassName('input');
+
+for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].style.display = "inline-block";
+    inputElements[i].value = "";
+}
+
+/*
 setTimeout(() => {
     for (let i = 0; i < possibleElements.length; i++) {
         possibleElements[i].style.display = "inline-block";
     }
-}, 1000);
+}, 1000);*/
